6c7837f41255c593ce07f3b3a996f93f70ab78e3
Matias Ruiz <matiasignacioruiz@gmail.com>
2022-09-07 10:28:12 -0300

    realizado el ejercicio 1

--- Py_Ejercicio_2/Cajero_auto.py
+++ Py_Ejercicio_2/Cajero_auto.py
@@ -1,20 +1,42 @@
 # Ejercicio: Cajero Automatico.
-cajero_automatico={
-    'Deposito':[],
-    'Extracción':[],
-    'Transferencia':[],
-    'Salir':"Gracias por utilizar nuestro cajero"
-}
-
-print("Bienvenido a su Cajero Automático.")
+cajero_automatico=[
+    'Deposito','Extracción','Transferencia','Salir'
+]
+menu="Bienvenido a su Cajero Automático."
+print(menu)
 opcion = int(input("Elija su operación:\n\t 1-Depositos,\n\t 2-Extracción,\n\t 3-Transferencia,\n\t 4-Salir\n"))
 
-if opcion == 1 :
-    print(cajero_automatico[0][0])
-elif opcion == 2:
-    print(cajero_automatico[1][0])
-elif opcion == 3:
-    print(cajero_automatico[2][0])
-elif opcion == 4:
-    print(cajero_automatico[3][0])
-else: print("No marco ninguna pción correcta")
+while opcion != 4:
+  
+    if opcion == 1 :
+        print(cajero_automatico[0])
+        dinero=input('\nIngrese un monto de dinero: \n\n')
+        print("Transfirió " + dinero + " correctamente\n\n")
+        print(menu)
+        opcion = int(input("Elija su operación:\n\t 1-Depositos,\n\t 2-Extracción,\n\t 3-Transferencia,\n\t 4-Salir\n"))
+       
+    elif opcion == 2:
+        print(cajero_automatico[1])
+        dinero=input('\nIngrese un monto de dinero: \n') 
+        print("Extrajo " + dinero + " correctamente\n\n")
+        print(menu)
+        opcion = int(input("Elija su operación:\n\t 1-Depositos,\n\t 2-Extracción,\n\t 3-Transferencia,\n\t 4-Salir\n"))
+
+    elif opcion == 3:
+        print(cajero_automatico[2])
+        dinero=input('\nIngrese un monto de dinero: \n\n')
+        print("Transfirió " + dinero + " correctamente\n\n")
+        print(menu)
+        opcion = int(input("Elija su operación:\n\t 1-Depositos,\n\t 2-Extracción,\n\t 3-Transferencia,\n\t 4-Salir\n"))
+
+    else: 
+        print("No marco ninguna pción correcta, seleccione una opción valida.\n")
+        print(menu)
+        opcion = int(input("Elija su operación:\n\t 1-Depositos,\n\t 2-Extracción,\n\t 3-Transferencia,\n\t 4-Salir\n"))
+
+print(cajero_automatico[3] + "\nAdiós")
+
+
+
+
+
